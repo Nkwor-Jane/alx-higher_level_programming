11c7e4cd1ce0d36d8d67b8ee036c43bd1646aa29
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-03-07 12:27:30 -0800

    Task on If else and functions

---
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+remove_char_at = __import__('101-remove_char_at').remove_char_at
+
+print(remove_char_at("Best SChool", 3))
+print(remove_char_at("Chicago", 2))
+
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            ltou = ord(str[i]) + ord('A') - ord('a')
+        else:
+            ltou = ord(str[i])
+        print(f"{chr(ltou)}", end='')
+    print()
