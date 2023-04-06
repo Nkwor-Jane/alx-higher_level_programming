5ec882a8a35246821dd9509b7b9ed1c8502f1763
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-04-05 21:48:39 -0700

    Everyting is an object -- Advanced tasks

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(my_string=[]):
+    my_string.append("Best School")
+    return (', '.join(my_string))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    """"class with no class or object attribute, that prevents a user
+   from dynamically creating new instance attributes excpt if the new
+   instance attribute is called first_name"""
+
+    __slots__ = ['first_name']
