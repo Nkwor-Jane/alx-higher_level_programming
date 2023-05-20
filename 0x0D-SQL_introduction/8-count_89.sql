cdd4eb61a288bffd6eeeaa92a945ed4b9e546175
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-05-20 10:37:59 -0700

    Made corrections to SQL introductions task

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- display number of record with id=89 in table first_table
-SELECT COUNT(id=89) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id=89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,19 +1,11 @@
 -- create a table second_table
-CREATE TABLE  IF NOT EXISTS second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
 
-INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score)
-VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score)
-VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score)
-VALUES (4, 'George', 8);
-INSERT INTO second_table (id, name, score)
-VALUES (5, 'Aria', 18);
-INSERT INTO second_table (id, name, score)
-VALUES (6, 'Aria', 10);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
